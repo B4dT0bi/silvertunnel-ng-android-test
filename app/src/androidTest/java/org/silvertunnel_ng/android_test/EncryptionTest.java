b7cdd037d4ec5e52abc09ac4e4ad652fc2ed2384
Tobi <tobias.boese@gmail.com>
2015-05-20 22:57:03 +0200

    Initial commit

---
+++ app/src/androidTest/java/org/silvertunnel_ng/android_test/EncryptionTest.java
@@ -0,0 +1,15 @@
+package org.silvertunnel_ng.android_test;
+
+import android.test.InstrumentationTestCase;
+
+import junit.framework.Assert;
+
+/**
+ * Created by b4dt0bi on 20.05.15.
+ */
+public class EncryptionTest extends InstrumentationTestCase {
+
+    public void test() {
+        Assert.assertEquals(true, true);
+    }
+}
